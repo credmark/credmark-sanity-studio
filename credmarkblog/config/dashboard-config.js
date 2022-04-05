9260d30d169fbd32aac997bf054c38fdd136e71c
Elektra Murphy <elektra.murphy@icloud.com>
2022-04-05 18:11:32 +0100

    Staging v2 button added

--- credmarkblog/config/dashboard-config.js
+++ credmarkblog/config/dashboard-config.js
@@ -18,6 +18,12 @@ export default {
               title: "Credmark [Staging]",
             },
             {
+              name: "staging-credmark-website-v2",
+              apiId: "24030de2-bcaf-4c94-b621-d4d106df402d",
+              buildHookId: "624c76b26f87757e736fe1ea",
+              title: "Credmark [Staging V2]",
+            },
+            {
               name: "credmark-website",
               apiId: "7456caf5-ee43-47c9-83f2-68389360f1c0",
               buildHookId: "61f6ba594c4a46e1091dbd0a",
